8cf382f98a8e831a9e57d91a77d1b72bc7c2044f
jiwonch <rizjw26@gmail.com>
2024-06-09 13:52:43 +0900

    Fix - exit를 입력받으면 종료

--- src/main/java/org/example/cjw/board/Main.java
+++ src/main/java/org/example/cjw/board/Main.java
@@ -1,10 +1,25 @@
 package org.example.cjw.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("게시판 시작");
-    System.out.println("프로그램 시작");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("자바 게시판 시작");
+
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.equals("exit")) {
+        break;
+      }
+
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
+    }
+
     System.out.println("프로그램 끝");
-    System.out.println("게시판 끝");
+    sc.close();
+    System.out.println("자바 게시판 끝");
   }
 }
