f26181c3cdad35126b89e6c85e1a4da28c06fdcc
jiwonch <rizjw26@gmail.com>
2024-06-09 13:37:17 +0900

    Fix - main file fix

--- src/main/java/org/example/cjw/board/Main.java
+++ src/main/java/org/example/cjw/board/Main.java
@@ -2,6 +2,9 @@ package org.example.cjw.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("게시판 시작");
+    System.out.println("프로그램 시작");
+    System.out.println("프로그램 끝");
+    System.out.println("게시판 끝");
   }
 }
