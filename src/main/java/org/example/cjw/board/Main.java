ad2dcdf1732b4f212e4cc61d10b35ea9e26dae94
jiwonch <rizjw26@gmail.com>
2024-06-09 14:24:52 +0900

    Fix 게시물 입력받은 수 만큼 id 올리기

--- src/main/java/org/example/cjw/board/Main.java
+++ src/main/java/org/example/cjw/board/Main.java
@@ -8,12 +8,17 @@ import java.util.Scanner;
  * 제목 : ~~~~~~ -> 실제로 입력받을 수 있도록
  * 내용 : ~~~~
  * 1번 게시물이 등록되었습니다.
- * 명령)
+ * 명령) /usr/article/write
+ * == 게시물 등록 ==
+ * 제목 : ~~~~~~ -> 실제로 입력받을 수 있도록
+ * 내용 : ~~~~
+ * 2번 게시물이 등록되었습니다.
  * */
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("자바 게시판 시작");
+    int id = 1;
 
     while (true) {
       System.out.print("명령) ");
@@ -26,16 +31,14 @@ public class Main {
         String title = sc.nextLine();
         System.out.print("내용 : ");
         String contents = sc.nextLine();
-        System.out.println("1번 게시물이 등록되었습니다.");
-        
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id++);
       } else if(cmd.equals("exit")) {
+        System.out.println("프로그램 끝");
         break;
       } else {
         System.out.println("올바른 명령어가 아닙니다.");
       }
     }
-
-    System.out.println("프로그램 끝");
     sc.close();
     System.out.println("자바 게시판 끝");
   }
