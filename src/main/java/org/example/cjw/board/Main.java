adc00463fdfeaa3e190017395251d2e69b5b4825
jiwonch <rizjw26@gmail.com>
2024-06-09 14:17:50 +0900

    C:/Program Files/Git/usr/article/write 입력받으면 제목, 내용 입력받기

--- src/main/java/org/example/cjw/board/Main.java
+++ src/main/java/org/example/cjw/board/Main.java
@@ -2,6 +2,14 @@ package org.example.cjw.board;
 
 import java.util.Scanner;
 
+/**
+ * 명령) /usr/article/write
+ * == 게시물 등록 ==
+ * 제목 : ~~~~~~ -> 실제로 입력받을 수 있도록
+ * 내용 : ~~~~
+ * 1번 게시물이 등록되었습니다.
+ * 명령)
+ * */
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -11,11 +19,20 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")) {
-        break;
-      }
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
 
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+        System.out.print("내용 : ");
+        String contents = sc.nextLine();
+        System.out.println("1번 게시물이 등록되었습니다.");
+        
+      } else if(cmd.equals("exit")) {
+        break;
+      } else {
+        System.out.println("올바른 명령어가 아닙니다.");
+      }
     }
 
     System.out.println("프로그램 끝");
